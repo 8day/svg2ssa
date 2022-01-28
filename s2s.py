61683a29f9943ae134814e5293922a3f47cb9151
8day <5513525+8day@users.noreply.github.com>
2022-01-28 16:58:13 +0200

    moved all s2s_main.py logic into s2s.py.

--- s2s.py
+++ s2s.py
@@ -1,8 +1,136 @@
+import re
+
+# Uncomment when cx_Freeze'ing with lxml.
+# from lxml import etree
+
+# Uncomment when cx_Freeze'ing with xml.etree.
+# from xml.etree import ElementTree as etree
+
+# Comment when cx_Freeze'ing to prevent loading of lxml even if xml.etree is needed.
+try:
+    from lxml import etree
+except ImportError:
+    # See: http://lxml.de/compatibility.html
+    from xml.etree import ElementTree as etree
+
+import s2s_runtime_settings
+from s2s_elems import SVGElement
+from s2s_utilities import convert_svglength_to_pixels
+
+
+class S2S:
+    """This is 'main()', if you like. Spins all the machinery behind it."""
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.element_stack = []
+        self.container_stack = []
+        self.ssa_meta = {}
+
+    @staticmethod
+    def make_round_and_mod(nmb, mod):
+        nmb = round(nmb)
+        if nmb % mod != 0:
+            nmb += mod - (nmb % mod)
+        return nmb
+
+    def start_event_for_g(self, atts):
+        curr = SVGElement.from_raw_data("g", atts)
+        try:
+            prev = self.container_stack[-1]
+            curr += prev
+        except IndexError:
+            pass
+        self.container_stack.append(curr)
+
+    def end_event_for_g(self):
+        if self.container_stack:
+            del self.container_stack[-1]
+
+    def start_event_for_path(self, atts):
+        curr = SVGElement.from_raw_data("path", atts)
+        try:
+            prev = self.container_stack[-1]
+            curr += prev
+        except IndexError:
+            pass
+        self.element_stack.append(curr)
+
+    def end_event_for_path(self):
+        pass
+
+    def start_event_for_svg(self, atts):
+        width = atts.get("width")
+        height = atts.get("height")
+        if width is not None and height is not None:
+            width = atts["width"]
+            height = atts["height"]
+            width = convert_svglength_to_pixels(width)
+            height = convert_svglength_to_pixels(height)
+            width = S2S.make_round_and_mod(width, 16)
+            height = S2S.make_round_and_mod(height, 16)
+        else:
+            width = s2s_runtime_settings.ssa_default_playresx
+            height = s2s_runtime_settings.ssa_default_playresx
+        self.ssa_meta["playresx"] = width
+        self.ssa_meta["playresy"] = height
+
+    def end_event_for_svg(self):
+        pass
+
+    start = dict(
+        path=start_event_for_path, g=start_event_for_g, svg=start_event_for_svg
+    )
+
+    end = dict(path=end_event_for_path, g=end_event_for_g, svg=end_event_for_svg)
+
+    def ssa_repr(self):
+        filepath = self.filepath
+        for action, element in etree.iterparse(filepath, ("start", "end")):
+            ns_name, local_name = re.search(r"^(\{.+?\})(.+)$", element.tag).group(1, 2)
+            if action == "start":
+                if local_name in S2S.start:
+                    S2S.start[local_name](self, element.attrib)
+            else:
+                if local_name in S2S.end:
+                    S2S.end[local_name](self)
+
+        ssa_table = []
+        for element in self.element_stack:
+            atts = element.ssa_repr()
+            ssa_table.append(
+                s2s_runtime_settings.ssa_event.format(
+                    actor=atts.pop("id"),
+                    trans=atts.pop("transform"),
+                    drwng=atts.pop("d"),
+                    m_lev=s2s_runtime_settings.magnification_level,
+                    codes="".join(obj for key, obj in atts.items()),
+                )
+            )
+        ssa_table = "\n".join(ssa_table)
+
+        ssa_header = s2s_runtime_settings.ssa_header.format(
+            width=self.ssa_meta["playresx"], height=self.ssa_meta["playresy"]
+        )
+        with open(filepath + ".ass", "w+t", buffering=65536) as fh:
+            fh.write(ssa_header)
+            fh.write("\n")
+            fh.write(ssa_table)
+            fh.write("\n")
+            print(
+                "Successfully converted:",
+                filepath if len(filepath) < 52 else "..." + filepath[-52:],
+            )
+
+        self.element_stack = []
+        self.container_stack = []
+        self.ssa_meta = {}
+
+
 if __name__ == "__main__":
     from sys import argv as sys_argv
     from os import path as os_path
     from argparse import ArgumentParser, REMAINDER
-    import s2s_runtime_settings
 
     parser = ArgumentParser(
         description="Converts SVG (Rec 1.1) into SSA (v4.0+).",
@@ -95,9 +223,6 @@ if __name__ == "__main__":
         "MarginL, MarginR, MarginV, Effect, Text"
     )
     s2s_runtime_settings.ssa_event = "Dialogue: 0,0:00:00.00,0:00:02.00,s2s.default,{actor},0000,0000,0000,,{{\\p{m_lev}{trans}{codes}}} {drwng} {{\\p0}}"
-
-    # Must be located in the very end, so that ``s2s_runtime_settings`` got populated.
-    from s2s_main import S2S
 
     if os_path.isfile(path):
         S2S(path).ssa_repr()
