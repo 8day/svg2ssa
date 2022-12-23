d916a698a23441322e64946744200138c9b7241d
8day <5513525+8day@users.noreply.github.com>
2022-12-23 20:42:45 +0200

    fixed "freezing" (forgot about relative paths and renamed script).

--- freeze_with_cx_Freeze.py
+++ freeze_with_cx_Freeze.py
@@ -13,17 +13,17 @@ from cx_Freeze.freezer import Freezer
 from tomlkit import parse
 
 
-with open("..\\pyproject.toml", "r+t", encoding="utf-8") as file:
+with open(".\\pyproject.toml", "r+t", encoding="utf-8") as file:
     pyproject = file.read()
 
 VERSION = parse(pyproject)["tool"]["poetry"]["version"]
 
 freezer = Freezer(
-    [Executable(".\\boot_svg2ssa.py", target_name="svg2ssa", icon="..\\logo.ico")],
+    [Executable(".\\boot_svg2ssa_during_freeze.py", target_name="svg2ssa", icon=".\\logo.ico")],
     packages=["xml.etree.ElementTree", "lxml.etree", "lxml._elementpath", "gzip", "defusedxml.ElementTree"],
-    targetDir=f"..\\dist\\svg2ssa-{VERSION}",
+    targetDir=f".\\dist\\svg2ssa-{VERSION}",
 )
 freezer.Freeze()
 
-make_archive(f"..\\dist\\svg2ssa-{VERSION}", "zip", "..\\dist", f".\\svg2ssa-{VERSION}")
-rmtree(f"..\\dist\\svg2ssa-{VERSION}")
+make_archive(f".\\dist\\svg2ssa-{VERSION}", "zip", ".\\dist", f".\\svg2ssa-{VERSION}")
+rmtree(f".\\dist\\svg2ssa-{VERSION}")
