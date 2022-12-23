abb5198c332199f42cb05e1bd15b9a38e8ddc8b8
8day <5513525+8day@users.noreply.github.com>
2022-12-23 20:36:27 +0200

    added important note.

--- boot_svg2ssa_during_freeze.py
+++ boot_svg2ssa_during_freeze.py
@@ -1,6 +1,10 @@
 """Script required for relative paths mechanism.
 
 Both :mod:`py2exe` and :mod:`cx_Freeze` require it, which implies that it's hard to bypass it.
+
+Warning:
+
+    Turns out script for booting of the package with relative imports must be in same dir as the package itself (hacking sys.path doesn't help, as that code is than baked into frozen release).
 """
 
 
